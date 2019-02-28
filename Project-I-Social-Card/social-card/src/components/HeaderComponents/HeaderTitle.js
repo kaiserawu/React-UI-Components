d0081f5620f69ae9d641754ca374ceb38fe79989
kaiserawu <kaiser.a.wu@gmail.com>
2019-02-27 20:24:36 -0800

    Created and styled header components

--- Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
+++ Project-I-Social-Card/social-card/src/components/HeaderComponents/HeaderTitle.js
@@ -1,2 +1,13 @@
 import React from 'react';
 import './Header.css';
+
+const HeaderTitle = () => {
+    return (
+        <div class="headerTitle">
+            <h2>Lambda School</h2>
+            <h3>@LambdaSchool • 26 jan</h3>
+        </div>
+    )
+}
+
+export default HeaderTitle;
