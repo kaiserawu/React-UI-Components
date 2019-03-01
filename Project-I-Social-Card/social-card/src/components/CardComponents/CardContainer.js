7e84688580789671e30e35d36460870e8590558b
kaiserawu <kaiser.a.wu@gmail.com>
2019-02-28 19:17:33 -0800

    Implement and style card component

--- Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
+++ Project-I-Social-Card/social-card/src/components/CardComponents/CardContainer.js
@@ -1,2 +1,18 @@
 import React from 'react';
 import './Card.css';
+
+import CardBanner from './CardBanner';
+import CardContent from './CardContent';
+
+const CardContainer = () => {
+    return (
+        <div class="cardContainer">
+            <CardBanner />
+            <div class="cardText">
+                <CardContent />
+            </div>
+        </div>
+    )
+}
+
+export default CardContainer;
